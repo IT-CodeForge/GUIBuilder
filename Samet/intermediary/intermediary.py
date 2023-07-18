b623e1dc7ad629a6dfffaa2a58e098062c477e32
svn <svn>
2023-07-18 10:50:21 +0000

    -

--- Samet/intermediary/intermediary.py
+++ Samet/intermediary/intermediary.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from multiprocessing import Event
 from intermediary.object.object_attribute import ObjectAttribute
 from intermediary.object.generic_object import GenericObject
 from intermediary.object.window_object import WindowObject
@@ -7,6 +8,14 @@ from intermediary.object.button_object import ButtonObject
 class ObjectEnum(Enum):
     WINDOW = 0
     BUTTON = 1
+
+class EventEnum(Enum):
+    Timer = 0
+    KeyUp = 1
+    KeyDown = 2
+    MouseMove = 3
+    MouseClick = 4
+    Paint = 5
 
 class Intermediary:
     def __init__(self) -> None:
@@ -22,6 +31,13 @@ class Intermediary:
 
         self.__objects: list[GenericObject] = []
         self.__count: int = 0
+
+        self.__timer_enabled = False
+        self.__key_up_enabled = False
+        self.__key_down_enabled = False
+        self.__mouse_move_enabled = False
+        self.__mouse_click_enabled = False
+        self.__paint_enabled = False
 
     def createObject(self, type: ObjectEnum) -> int:
         object_type: type = self.__enum_mapping.get(type)
@@ -53,9 +69,10 @@ class Intermediary:
 
         print(f"Error: An object with the ID {id} could not be retrieved.")
 
-    def setObjects(self, objects: list[dict[str, any]]) -> None:
+    def loadObjects(self, objects: list[dict[str, any]]) -> None:
         # Clear all objects.
         self.__objects = []
+        self.__count = 0
 
         for object in objects:
             object_type: type = self.__string_mapping.get(object["type"])
@@ -86,3 +103,51 @@ class Intermediary:
             objects.append(dictionary)
             
         return objects
+
+    def setEvent(self, type: EventEnum) -> None:
+        if type == EventEnum.Timer:
+            self.__timer_enabled = True
+        elif type == EventEnum.KeyUp:
+            self.__key_up_enabled = True
+        elif type == EventEnum.KeyDown:
+            self.__key_down_enabled = True
+        elif type == EventEnum.MouseMove:
+            self.__mouse_move_enabled = True
+        elif type == EventEnum.MouseClick:
+            self.__mouse_click_enabled = True
+        elif type == EventEnum.Paint:
+            self.__paint_enabled = True
+
+    def clearEvent(self, type: EventEnum) -> None:
+        if type == EventEnum.Timer:
+            self.__timer_enabled = False
+        elif type == EventEnum.KeyUp:
+            self.__key_up_enabled = False
+        elif type == EventEnum.KeyDown:
+            self.__key_down_enabled = False
+        elif type == EventEnum.MouseMove:
+            self.__mouse_move_enabled = False
+        elif type == EventEnum.MouseClick:
+            self.__mouse_click_enabled = False
+        elif type == EventEnum.Paint:
+            self.__paint_enabled = False
+
+    def getEvents(self) -> dict[str, bool]:
+        dictionary: dict[str, bool] = {}
+
+        dictionary["timer_enabled"] = self.__timer_enabled
+        dictionary["keyup_enabled"] = self.__key_up_enabled
+        dictionary["keydown_enabled"] = self.__key_down_enabled
+        dictionary["mousemove_enabled"] = self.__mouse_move_enabled
+        dictionary["mouseclick_enabled"] = self.__mouse_click_enabled
+        dictionary["paint_enabled"] = self.__paint_enabled
+
+        return dictionary
+
+    def loadEvents(self, events: dict[str, bool]) -> None:
+        self.__timer_enabled = events["timer_enabled"]
+        self.__timer_enabled = events["keyup_enabled"]
+        self.__timer_enabled = events["keydown_enabled"]
+        self.__timer_enabled = events["mousemove_enabled"]
+        self.__timer_enabled = events["mouseclick_enabled"]
+        self.__timer_enabled = events["paint_enabled"]
--- Samet/intermediary/json.py
+++ Samet/intermediary/json.py
@@ -8,13 +8,26 @@ class JSON:
     def load(self, name: str) -> None:
         objects: list[dict[str, any]] = []
 
-        with open(f"{name}.json", "r") as file:
+        with open(f"{name}_objects.json", "r") as file:
             objects = json.loads(file.read())
 
-        self.__intermediary.setObjects(objects)
+        self.__intermediary.loadObjects(objects)
+
+        events: dict[str, bool] = {} 
+
+        with open(f"{name}_events.json", "r") as file:
+            events = json.loads(file.read())
+
+        self.__intermediary.loadEvents(events)
 
     def save(self, name: str) -> None:
         objects: list[dict[str, any]] = self.__intermediary.getObjects()
 
-        with open(f"{name}.json", "w") as file:
+        with open(f"{name}_objects.json", "w") as file:
             file.write(json.dumps(objects, indent = 4))
+
+        events: dict[str, bool] = self.__intermediary.getEvents()
+
+        with open(f"{name}_events.json", "w") as file:
+            file.write(json.dumps(events, indent = 4))
+        
