4cf40375a9d6ebbbdedc48f7a7c2343e5c73d1d7
svn <svn>
2023-07-21 07:43:51 +0000

    -

--- Louis/Generator/generator.py
+++ Louis/Generator/generator.py
@@ -1,6 +1,6 @@
-from difflib import restore
+#add .get() for events
 import os.path
-from turtle import st
+
 
 class Generator:
     def __init__(self) -> None:
@@ -44,46 +44,40 @@ class Generator:
 
         ret_str += self.__generate_attributes(objects)
         
-        ret_str += 'public:\n  GUI();\n  void eventMouseMove(int posX, int posY);\n'
+        ret_str += 'private:\n'
         for object in objects:
             if object["type"] == "window":
                 continue
-            if object["eventPressed"]:
+            if object.get("eventPressed", False):
                 ret_str += "  void event_pressed_" + object["name"] + "(int event);\n"
-            if object["eventHovered"]:
+            if object.get("eventHovered", False):
                 ret_str += "  void event_hovered_" + object["name"] + "();\n"
-            if object["eventChanged"]:
+            if object.get("eventChanged", False):
                 ret_str += "  void event_changed_" + object["name"] + "();\n"
-        ret_str += 'private:\n'
-        ret_str += "  void hoveredEvent();\n"
+        #ret_str += "  void eventHovered(void* id);\n" | hovered vorerst gescrapt
+        ret_str += 'public:\n  GUI();\n  void eventMouseMove(int posX, int posY);\n'
         ret_str += '  void eventButton(TGWButton* einButton, int event);\n'
-        ret_str += '  void eventTimer(TGWMainWindow* messageHandlerWindow, int intervallMilliSeconds, int* id);\n'
+        ret_str += '  void eventTimer(int id);\n'
         ret_str += '};\n\n#endif'
         return ret_str
     
     def __generate_cpp(self, objects: list[dict[str, any]]) -> str:
-        ret_str  = '#include "GUI.h"\n#include "TGW_AllClassDefinitions.h"\n#include "TGWTimer"\n\n'
+        ret_str  = '#include "GUI.h"\n#include "TGW_AllClassDefinitions.h"\n#include "TGWTimer.h"\n\n'
         for object in objects:
             if object["type"] == "window":
                 ret_str += '\nGUI::GUI()\n  :TGWMainWindow('
-                ret_str += str(object["position"][0]) + ", " + str(object["position"][1]) + ", "
+                ret_str += "10, 10, "
                 ret_str += str(object["size"][0]) + ", " + str(object["size"][1]) + ", "
                 ret_str += '"' + str(object["text"]) + '", '
                 ret_str += "0x" + self.__hex_color_converter(object["backgroundColor"][0]) + self.__hex_color_converter(object["backgroundColor"][1])+ self.__hex_color_converter(object["backgroundColor"][2]) + ')\n{\n'
                 break
         
-        ret_str += "  hoverEvent = new TGWTimer(this, 1, &hoverTimer);\n"
-        for object in objects:
-            if object["type"] == "window":
-                continue
-            ret_str += self.offset + object["name"] + " = " + self.__generate_cpp_object(object)
-        ret_str += self.function_end + "\n"
-
+        ret_str += self.__generate_constructor(objects)
 
         ret_str += self.__generate_btn_event(objects)
-        ret_str += self.__generate_moumo_event()
+        ret_str += self.__generate_moumo_event(objects)
         ret_str += self.__generate_tim_event(objects)
-        ret_str += self.__generate_hov_event(objects)
+        #ret_str += self.__generate_hov_event(objects) | hovered vorerst gescrapt
         return ret_str
     
     def __generate_cpp_user(self, path: str, objects: list[dict[str, any]]) -> str:
@@ -104,7 +98,8 @@ class Generator:
             for object in objects:
                 if object["type"] == "window":
                     continue
-                if event_name == object["name"] and ((event_type == "pressed" and object["eventPressed"]) or (event_type == "hovered" and object["eventHovered"]) or (event_type == "changed" and object["eventChanged"])):
+                type_checking = (event_type == "pressed" and object.get("eventPressed", False)) or (event_type == "hovered" and object.get("eventHovered", False)) or (event_type == "changed" and object.get("eventChanged", False))
+                if event_name == object["name"] and (type_checking):
                     is_in_list = True
                     already_added_events.append([event_name, event_type])
             if not is_in_list:
@@ -115,13 +110,130 @@ class Generator:
         for object in objects:
             if object["type"] == "window":
                 continue
-            if [object["name"], "pressed"] not in already_added_events and object["eventPressed"]:
+            if [object["name"], "pressed"] not in already_added_events and object.get("eventPressed", False):
                         ret_str += "void GUI::event_pressed_" + object["name"] + "(int event)\n{\n}\n\n"
-            if [object["name"], "hovered"] not in already_added_events and object["eventHovered"]:
+            if [object["name"], "hovered"] not in already_added_events and object.get("eventHovered", False):
                         ret_str += "void GUI::event_hovered_" + object["name"] + "()\n{\n}\n\n"
-            if [object["name"], "changed"] not in already_added_events and object["eventChanged"]:
+            if [object["name"], "changed"] not in already_added_events and object.get("eventChanged", False):
                         ret_str += "void GUI::event_changed_" + object["name"] + "()\n{\n}\n\n"
         
+        return ret_str
+
+
+
+
+
+    
+
+    def __generate_constructor(self, objects: list[dict[str, any]]) -> str:
+        ret_str = ""
+        for object in objects:
+            if object["type"] == "window":
+                continue
+            ret_str += self.offset + object["name"] + " = " + self.__generate_cpp_object(object)
+        ret_str += self.function_end + "\n"
+        for object in objects:
+            if object["type"] == "window":
+                continue
+            if "backgroundColor" in object:
+                ret_str += "" #macht was sobald man die Farbe in Hallmans image sten kann|self.offset + object["name"] + "->set"
+        return ret_str
+
+    
+    def __generate_attributes(self, objects: list[dict[str, any]]) -> str:
+        ret_str = ""
+        for object in objects:
+            if object["type"] == "window":
+                continue
+            ret_str += self.offset + self.type_translation[object["type"]] + "* " + object["name"] + ";\n"
+        
+        ret_str += "\n"
+        
+        ret_str += "  int mouseX = 0;\n  int mouseY = 0;\n"
+        return ret_str
+
+
+    def __generate_btn_event(self, objects: list[dict[str, any]]) -> str:
+        ret_str = "\nvoid GUI::eventButton(TGWButton* einButton, int event)\n{\n"
+        for object in objects:
+            if object["type"] == "button" and object["eventPressed"]:
+                ret_str += "  if(einButton == this->" + object["name"] + ")\n  {\n"
+                ret_str += "    event_pressed_" + object["name"] + "(event);\n  }\n"
+        ret_str += self.function_end + "\n"
+        return ret_str
+    
+    def __generate_tim_event(self, objects: list[dict[str, any]]) -> str:
+        ret_str  = "\nvoid GUI::eventTimer(int id)\n{\n"
+        for object in objects:
+            if object["type"] == "timer":
+                ret_str += "  if(id == " + object["name"] + "Id)\n  {\n"
+                ret_str += "    event_timer_" + object["name"] + "();\n  }\n"
+        ret_str += self.function_end + "\n"
+        return ret_str
+    
+    def __generate_hov_event(self, objects: list[dict[str, any]]) -> str:
+        ret_str  = "void GUI::eventHovered(void* id)\n{\n"
+        for object in objects:
+            if object["type"] == "window":
+                continue
+            if object.get("eventHovered", False):
+                ret_str += "  if(id == " + object["name"] + ")\n  {\n"
+                ret_str += "    event_hovered_" + object["name"] + "();\n  }\n"
+        ret_str += self.function_end + "\n"
+        return ret_str
+    
+    def __generate_moumo_event(self, objects: list[dict[str, any]]) -> str:
+        ret_str = "\nvoid GUI::eventMouseMove(int posX, int posY)\n{\n  ""\n}\n"
+        return ret_str
+    
+    def __generate_parsed_events(self, objects: list[dict[str, any]]) -> str:
+        ret_str = ""
+        for object in objects:
+            if object.get("type") != "window":
+                continue
+            if "eventCreate" in object:
+                ret_str += "\nvoid GUI::eventShow()\n{\n  event"
+            if "eventPaint" in object:
+                pass
+            if "eventResize" in object:
+                pass
+            if "eventMouseClick" in object:
+                pass
+            if "eventMouseMove" in object:
+                ret_str += "\nvoid GUI::eventMouseMove(int posX, int posY)\n{\n  event_Mousmov(posX, posY);\n}\n"
+
+	
+	
+
+
+
+    def __generate_button(self, object: dict[str, any]) -> str:
+        ret_str  = ""
+        ret_str += "new " + self.type_translation[object["type"]] + "(this, "
+        ret_str += str(object["position"][0]) + ", " + str(object["position"][1]) + ", "
+        ret_str += str(object["size"][0]) + ", " + str(object["size"][1]) + ", "
+        ret_str += '"' + object["text"] + '");\n'
+        return ret_str
+    
+    
+
+    def __check_user_includes(self, objects: list[dict[str, any]]) -> str:
+        ret_str = ""
+        unique_type_list = []
+        for object in objects:
+            if object["type"] == "button" and "button" not in unique_type_list:
+                unique_type_list.append("button")
+        
+        for type in unique_type_list:
+            ret_str += '#include "' + self.type_translation[type] + '.h"\n'
+        return ret_str
+    
+    
+    def __generate_cpp_object(self, object: dict[str, any]) -> str:
+        ret_str = ""
+        if object["type"] == "button":
+            ret_str += self.__generate_button(object)
+            return ret_str
         return ret_str
     
 
@@ -164,103 +276,13 @@ class Generator:
         return method_contents
 
 
-
-
-
-    def __check_user_includes(self, objects: list[dict[str, any]]) -> str:
-        ret_str = ""
-        unique_type_list = []
-        for object in objects:
-            if object["type"] == "button" and "button" not in unique_type_list:
-                unique_type_list.append("button")
-        
-        for type in unique_type_list:
-            ret_str += '#include "' + self.type_translation[type] + '.h"\n'
-        return ret_str
-    
-    
-    def __generate_cpp_object(self, object: dict[str, any]) -> str:
-        ret_str = ""
-        if object["type"] == "button":
-            ret_str += self.__generate_button(object)
-            return ret_str
-        return ret_str
-    
-
-    def __generate_attributes(self, objects: list[dict[str, any]]) -> str:
-        ret_str = "  TGWTimer* hoverTimer = 1;\n"
-        for object in objects:
-            if object["type"] == "window":
-                continue
-            ret_str += self.offset + self.type_translation[object["type"]] + "* " + object["name"] + ";\n"
-        
-        ret_str += "\n"
-        
-        for object in objects:
-            if object["type"] == "window":
-                continue
-            if object["eventHovered"]:
-                x1 = object["position"][0]
-                y1 = object["position"][1]
-                x2 = object["position"][0] + object["size"][0]
-                y2 = object["position"][1] + object["size"][1]
-                ret_str += "  int " + object["name"] + "Id[4] = {" + f"{x1},{y1},{x2},{y2}" + "};\n"
-        ret_str += "  int mouseX = 0;\n  int mouseY = 0;\n"
-        return ret_str
-
-
-    def __generate_btn_event(self, objects: list[dict[str, any]]) -> str:
-        ret_str = "\nvoid GUI::eventButton(TGWButton* einButton, int event)\n{\n"
-        for object in objects:
-            if object["type"] == "button" and object["eventPressed"]:
-                ret_str += "  if(einButton == this->" + object["name"] + ")\n  {\n"
-                ret_str += "    event_pressed_" + object["name"] + "(event);\n  }\n"
-        ret_str += self.function_end + "\n"
-        return ret_str
-    
-    def __generate_tim_event(self, objects: list[dict[str, any]]) -> str:
-        ret_str  = "\nvoid GUI::eventTimer(int id)\n{\n"
-        ret_str += "  if(id == hoveredId)\n  {\n"
-        ret_str += "    checkHovered();\n  }\n"
-        for object in objects:
-            if object["type"] == "timer":
-                ret_str += "  if(id == " + object["name"] + "Id)\n  {\n"
-                ret_str += "    event_timer_" + object["name"] + "();\n  }\n"
-        ret_str += self.function_end + "\n"
-        return ret_str
-    
-    def __generate_hov_event(self, objects: list[dict[str, any]]) -> str:
-        ret_str  = "void GUI::hoveredEvent()\n{\n"
-        for object in objects:
-            if object["type"] == "window":
-                continue
-            if object["eventHovered"]:
-                ret_str += "  if(mouseX >=" + object["name"] + "Id[0] && mouseX <= " + object["name"] + "Id[2] && mouseY >= " + object["name"] + "Id[1] && mouseY <= " + object["name"] + "Id[3])\n  {\n"
-                ret_str += "    event_hovered_" + object["name"] + "();\n  }\n"
-        ret_str += self.function_end + "\n"
-        return ret_str
-    
-    def __generate_moumo_event(self) -> str:
-        ret_str = "\nvoid GUI::eventMouseMove(int posX, int posY);\n{\n  mouseX = posX;\n  mouseY = posY;\n}\n"
-        return ret_str
-
-
-    def __generate_button(self, object: dict[str, any]) -> str:
-        ret_str  = ""
-        ret_str += "new " + self.type_translation[object["type"]] + "(this, "
-        ret_str += str(object["position"][0]) + ", " + str(object["position"][1]) + ", "
-        ret_str += str(object["size"][0]) + ", " + str(object["size"][1]) + ", "
-        ret_str += '"' + object["text"] + '");\n'
-        return ret_str
-
-
         
 
 
     
 if __name__ == "__main__":
     myGenerator = Generator()
-    objects = [{"type": "window", "position": [10,10], "size": [1024,512], "text": "Hallo", "backgroundColor": [12,23,34]},{"type": "button", "name": "einButton", "position": [10,10], "size": [128,64], "text": "Knopf", "eventPressed": True, "eventHovered": True, "eventChanged": False},{"type": "button", "name": "einButton2", "position": [10,30], "size": [128,64], "text": "Knopf2", "eventPressed": True, "eventHovered": False, "eventChanged": False}]
+    objects = [{"type": "window", "position": [10,10], "size": [1024,512], "text": "Hallo", "backgroundColor": [12,23,34]},{"type": "button", "name": "einButton", "position": [10,10], "size": [128,64], "text": "Knopf", "eventPressed": True, "eventChanged": False},{"type": "button", "name": "einButton2", "position": [10,30], "size": [128,64], "text": "Knopf2", "eventPressed": True, "eventHovered": False, "eventChanged": False}]
     myGenerator.write_files("", objects)
 
 #,{"type": "button", "name": "einButton", "position": [10,10], "size": [128,64], "text": "Knopf", "eventPressed": True, "eventHovered": False, "eventChanged": False},{"type": "button", "name": "einButton2", "position": [10,30], "size": [128,64], "text": "Knopf2", "eventPressed": True, "eventHovered": False, "eventChanged": False}
