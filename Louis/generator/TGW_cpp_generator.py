33c822b79b36c1fa4ffabddcea01f503c27e3263
svn <svn>
2023-07-24 08:40:32 +0000

    -

--- Louis/generator/TGW_cpp_generator.py
+++ Louis/generator/TGW_cpp_generator.py
@@ -68,7 +68,7 @@ class TGW_cpp_generator:
     def __generate_changed_event(self, objects: list[dict[str, any]]) -> str:
         ret_str: str  = "void GUI::eventCheckBox(TGWCheckBox* eineCheckBox, int isChecked_1_0)\n{\n"
 
-        for object in objects:
+        for object in objects and object["eventChanged"]:
 
             if object["type"] == "checkbox":
                 ret_str += "  if(eineCheckBox == this->" + object["name"] + ")\n  {\n"
@@ -78,7 +78,7 @@ class TGW_cpp_generator:
 
         for object in objects:
 
-            if object["type"] == "edit":
+            if object["type"] == "edit" and object["eventChanged"]:
                 ret_str += "  if(einEdit == this->" + object["name"] + ")\n  {\n"
                 ret_str += "    event_changed_" + object["name"] + "();\n  }\n"
         ret_str += "}\n"
---
+++ Louis/test.py
@@ -0,0 +1,12 @@
+from generator.TGW_generator import*
+if __name__ == "__main__":
+    #code which tests evry functionality
+    myGenerator = TGW_Generator()
+    objects = [{"type": "window", "position": [10,10], "size": [1024,512], "text": "Hallo", "backgroundColor": [12,23,34], "eventMouseMove": True},
+               {"type": "button", "name": "einButton", "position": [10,10], "size": [128,32], "text": "Knopf", "eventPressed": True, "eventChanged": False},
+               {"type": "checkbox", "name": "meineCheckbox", "position": [10,52], "size": [128,32], "text": "ich bin eine Checkbox", "eventChanged": True, "checked": False},
+               {"id": 0, "type": "timer", "name": "einTimer", "interval": 1000, "enabled": True},
+               {"type": "canvas", "name": "einCanvas", "position": [148,10], "size": [138,138], "backgroundColor": [255,0,0]},
+               {"type": "label", "name": "einLabel", "position": [10, 94], "size": [12, 128], "text": "Ich bin ein label"},
+               {"type": "edit", "name": "Edit2", "position": [148, 94], "size": [12, 128], "text": "Ich bin ein edit", "multipleLines": True, "eventChanged": True}]
+    myGenerator.write_files("", objects)
