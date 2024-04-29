4ca4cfcc450b0a2626e3cbe7b749dae426e73f67
svn <svn>
2024-04-29 14:50:12 +0000

    -

--- _V2/GUIBUILDER/src/ETK.py
+++ _V2/GUIBUILDER/src/ETK.py
@@ -1,7 +1,13 @@
 """Diese Datei hat alle vom Endnutzer benötigten Module von ETK importiert. Der Endnutzer kann diese von dieser Datei einzeln oder mit * alle existierenden importieren."""
 
 
-from ETKV2.Internal.ETKBaseObject import ETKBaseEvents
+from ETKV2.Internal.ETKBaseObject import ETKBaseEvents, ETKBaseObject
+from ETKV2.Internal.ETKBaseWidget import ETKBaseWidget
+from ETKV2.Internal.ETKBaseTkObject import ETKBaseTkObject
+from ETKV2.Internal.ETKBaseTkWidget import ETKBaseTkWidget, ETKBaseWidget
+from ETKV2.Internal.ETKBaseWidgetDisableable import ETKBaseWidgetDisableable
+from ETKV2.Internal.ETKBaseTkWidgetDisableable import ETKBaseTkWidgetDisableable
+from ETKV2.Internal.ETKBaseTkWidgetText import ETKBaseTkWidgetText
 from ETKV2.ETKBitmap import ETKBitmap
 from ETKV2.ETKButton import ETKButton, ETKButtonEvents
 from ETKV2.ETKCanvas import ETKCanvas
@@ -12,7 +18,7 @@ from ETKV2.ETKCanvasOval import ETKCanvasOval
 from ETKV2.ETKCanvasRectangle import ETKCanvasRectangle
 from ETKV2.ETKCanvasSquare import ETKCanvasSquare
 from ETKV2.ETKCheckbox import ETKCheckbox, ETKCheckboxEvents
-from ETKV2.Internal.ETKBaseContainer import ETKAlignments, ETKContainerSize
+from ETKV2.Internal.ETKBaseContainer import ETKAlignments, ETKContainerSize, ETKBaseContainer
 from ETKV2.ETKContainer import ETKContainer
 from ETKV2.ETKEdit import ETKEdit, ETKEditEvents
 from ETKV2.ETKLabel import ETKLabel
@@ -52,3 +58,11 @@ if __name__ == "__main__":
     vector2d.mro
     ETKDropdownMenu.abs_enabled
     ETKDropdownMenuEvents.CHANGED
+    ETKBaseContainer.abs_enabled
+    ETKBaseObject.abs_pos
+    ETKBaseWidget.abs_enabled
+    ETKBaseTkObject.abs_pos
+    ETKBaseTkWidget.abs_enabled
+    ETKBaseWidgetDisableable.abs_enabled
+    ETKBaseTkWidgetDisableable.abs_enabled
+    ETKBaseTkWidgetText.abs_enabled
