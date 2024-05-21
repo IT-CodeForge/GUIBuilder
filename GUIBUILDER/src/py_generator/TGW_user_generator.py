83cf3d93f061d4645f2783488fbc11cfbe31ec4d
svn <svn>
2024-05-21 16:11:19 +0000

    -

--- GUIBUILDER/src/py_generator/TGW_user_generator.py
+++ GUIBUILDER/src/py_generator/TGW_user_generator.py
@@ -7,10 +7,11 @@ from typing import Optional
 
 class TGW_user_generator(BaseTGWGenerator):
     __VALID_FUNC_NAME_CHARACTER: list[str] = [
-        "_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
         "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
         "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+        "_", "1", "2", "3", "4", "5", "6", "7", "8", "9"
         ]
     def __init__(self) -> None:
         super().__init__()
