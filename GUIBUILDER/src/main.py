86cd74a0ecfb8cc14f00af96a33c315b9913671b
svn <svn>
2024-05-25 22:53:07 +0000

    -

--- GUIBUILDER/src/main.py
+++ GUIBUILDER/src/main.py
@@ -6,12 +6,10 @@ import time
 from steuerung import Steuerung
 
 version = "0.1"
-dir_root: str
-internal_dir_root: str
+dir_root: str  # path to root dir (project folder / folder of exe)
+internal_dir_root: str  # path to internal root dir (project folder / folder of unpacked files)
 additional_files_path: str
 
-# from io import TextIOWrapper
-# from os import fsync, path
 
 class MSGBoxStream():
     def __init__(self):
@@ -24,7 +22,6 @@ class MSGBoxStream():
         msgbox.create_msg_box(
             f"GUI-Builder - FEHLER", self.msg, msgbox.BUTTON_STYLES.OK)
         self.msg = ""
-        
 
     def write(self, text: str) -> int:
         self.msg += text
@@ -32,28 +29,30 @@ class MSGBoxStream():
             self.t = Thread(target=self.__send)
             self.t.start()
             import ctypes
-            ctypes.windll.shcore.SetProcessDpiAwareness(0) # NOTE
+            ctypes.windll.shcore.SetProcessDpiAwareness(0)  # NOTE
         return len(text)
 
     def flush(self) -> None:
         return
 
+
 if __name__ == "__main__":
     import main
-    freeze_support()
+    freeze_support()  # must be called to ensure that multiprocessing works correctly if packaged to exe (ignored when run as script)
+
+    # fixes problem when application is ran without console (because then sys.stdout and sys.stderr are nonexisting and any print() etc. raises an Error)
+    if sys.stdout is None:  # type:ignore
+        sys.stdout = open(devnull, "w")  # redirects output to void
+    if sys.stderr is None:  # type:ignore
+        sys.stderr = MSGBoxStream()  # redirects errors to MSGBox
 
     # Überprüft ob es als exe oder als py-script vorliegt
-    if path.split(sys.executable)[1] in ["python.exe", "pythonw.exe"]:
+    if not getattr(sys, "frozen", False):
         main.dir_root = path.abspath(path.join(path.split(path.abspath(__file__))[0], ".."))
         main.internal_dir_root = main.dir_root
     else:
-        # Release: Disables print(), etc
-        sys.stdout = open(devnull, 'w')
-        sys.stderr = MSGBoxStream()
-        # sys.stdout = FileAutoSave(r"C:\jk\out.log")
-        # sys.stderr = FileAutoSave(r"C:\jk\err.log")
         main.dir_root = path.split(path.abspath(sys.executable))[0]
-        main.internal_dir_root = path.split(path.abspath(__file__))[0]
+        main.internal_dir_root = str(sys._MEIPASS)  # type:ignore
     main.additional_files_path = path.abspath(f"{main.internal_dir_root}\\additional_files")
 
     print("\n"*20)
